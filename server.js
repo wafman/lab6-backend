f276232acc6896fef2012e08af051a7033be8f5e
river-ceanne <snvencer@gmail.com>
2019-04-30 10:22:05 -0700

    aving...

--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors'); app.use(cors());
-const app = express();
+const cors = require('cors');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./public'));
+app.use(cors());
+
+// app.use(express.static('./public'));
+
 
 app.get('/hello', (request, response) => {
   response.status(200).send('Hello');
@@ -18,10 +21,10 @@ app.get('/data', (request, response) => {
     departure: Date.now(),
     canFly: true,
     pilot: 'Well Trained'
-  }
+  };
   response.status(200).json(airplanes);
 });
 
-app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
+app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
